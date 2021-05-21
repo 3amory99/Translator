0627d41e41c328a58a7e376d904d2f02edc91bea
Omar Mahmoud <mora50017@gmail.com>
2021-05-21 22:04:45 +0200

    initial commit

---
+++ translator.py
@@ -0,0 +1,32 @@
+import sys
+from plyer import *
+from googletrans import Translator
+import time
+import os
+import pyperclip
+
+
+# method of translation process
+def translation(text):
+    # initiate instance
+    trans_item = Translator()
+    var = trans_item.translate(text, dest='ar')
+    notification.notify(
+        title=var.origin,
+        message=var.text,
+        app_name='Translator',
+        ticker='',
+        app_icon='logo.ico',
+        timeout=10
+    )
+
+
+sys.path.append(os.path.abspath("SO_site-packages"))
+current_text = ""
+while True:
+    # temporary value of clipboard chash memory
+    tmp_text = pyperclip.paste()
+    if tmp_text != current_text:
+        current_text = tmp_text
+        translation(current_text)
+    time.sleep(0.1)
